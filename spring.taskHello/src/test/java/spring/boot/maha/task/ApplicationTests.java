740d2f56eacd3d7ade64fca3b2d6394eb14bcd53
Mahajayanthi <94959966+Mahajayanthi@users.noreply.github.com>
2022-03-14 11:00:04 +0530

    Update ApplicationTests.java

--- spring.taskHello/src/test/java/spring/boot/maha/task/ApplicationTests.java
+++ spring.taskHello/src/test/java/spring/boot/maha/task/ApplicationTests.java
@@ -1,13 +1,13 @@
-package spring.boot.maha.task;
+// package spring.boot.maha.task;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApplicationTests {
+// @SpringBootTest
+// class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }

e6798b8af27dd594146736df988b17a8dfadb120
Mahajayanthi <94959966+Mahajayanthi@users.noreply.github.com>
2022-03-14 10:14:07 +0530

    Update calculatorTest.java

--- com.stg.junitproject/src/test/java/com/stg/junitproject/calculatorTest.java
+++ com.stg.junitproject/src/test/java/com/stg/junitproject/calculatorTest.java
@@ -1,44 +1,44 @@
-package com.stg.junitproject;
-import static org.junit.Assert.*;
+// package com.stg.junitproject;
+// import static org.junit.Assert.*;
 
-import java.rmi.AccessException;
+// import java.rmi.AccessException;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-public class calculatorTest {
+// import org.junit.After;
+// import org.junit.Before;
+// import org.junit.BeforeClass;
+// import org.junit.Test;
+// public class calculatorTest {
 	
-	  @Test
+// 	  @Test
 	
-	  public void test() {
-		  calculator c=new calculator();
-		  int actual =c.add(2, 3);
-		  int excepted=5;
-		  assertEquals(excepted,actual); } 
-	  @Test
-	  public void divideTest()  {
-		  calculator c=new calculator(); 
-		  int i=4; int j=0;
-		  int excepted=2;
-		  int actual=c.divide(4, 0);
-		  assertEquals(excepted,actual); }
-	@Test
-	  public void accountTest() { 
-		  account a=new account();
-		  int accountbalance=1000;
-	  int actual=a.accountbalance(); int excepted=1000;
-	  assertEquals(excepted,actual);
-	  }	
-	 @BeforeClass
-	 public  static void test11()
-	 {
-		 System.out.println("before testing");
-	 }
-	@After 
-	public void test1()
-	{
-		System.out.println("after testing");
-	}
+// 	  public void test() {
+// 		  calculator c=new calculator();
+// 		  int actual =c.add(2, 3);
+// 		  int excepted=5;
+// 		  assertEquals(excepted,actual); } 
+// 	  @Test
+// 	  public void divideTest()  {
+// 		  calculator c=new calculator(); 
+// 		  int i=4; int j=0;
+// 		  int excepted=2;
+// 		  int actual=c.divide(4, 0);
+// 		  assertEquals(excepted,actual); }
+// 	@Test
+// 	  public void accountTest() { 
+// 		  account a=new account();
+// 		  int accountbalance=1000;
+// 	  int actual=a.accountbalance(); int excepted=1000;
+// 	  assertEquals(excepted,actual);
+// 	  }	
+// 	 @BeforeClass
+// 	 public  static void test11()
+// 	 {
+// 		 System.out.println("before testing");
+// 	 }
+// 	@After 
+// 	public void test1()
+// 	{
+// 		System.out.println("after testing");
+// 	}
 
-}
+// }
